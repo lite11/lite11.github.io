33d904dfc16ebba637e16e95506054c9985f8a6c
lite11 <lite3256@gmail.com>
2016-06-07 12:18:56 +0530

    Update app_1.js

--- app_1.js
+++ app_1.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
      
-    var _API_KEY="AIzaSyC85aC3LWoMmHLdZMiVJVb8afENp02_Cf0";
+    var _API_KEY="AIzaSyBWOyG2WTttRhUh8l5IDpkGudRiQJ0bFpE";
 
   navigator.serviceWorker.register('sw.js').then(function(r) {
       console.log(r);
